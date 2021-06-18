d40ecefcd448f5c1c85b6e78cdd4ad4e6dd68ecd
Tjorven Beckedorf <tjorven.beckedorf@student.ibs-ol.de>
2021-06-18 15:52:54 +0200

    Initial Commit

---
+++ DartSwiftUI/DartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartSwiftUIApp.swift
+//  DartSwiftUI
+//
+//  Created by Tejay Beckedorf on 18.06.21.
+//
+
+import SwiftUI
+
+@main
+struct DartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
